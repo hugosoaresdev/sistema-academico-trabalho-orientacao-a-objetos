e340903ef99a09b2cc28c1600a039e3cf0f75d80
Petrônio Jr. <petronio.junior@sou.unifal-mg.edu.br>
2026-06-18 19:27:58 -0300

    tentando concertar erro de compilação

--- src/main/java/org/example/domain/Teacher.java
+++ src/main/java/org/example/domain/Teacher.java
@@ -1,12 +1,14 @@
 package org.example.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "registrationNumber")
 
 public class Teacher {
 
