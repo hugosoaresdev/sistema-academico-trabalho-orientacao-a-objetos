bca0599c88f515c96d250b7837fb503325aa7afa
Hugo Soares <hugosoareslopes1@gmail.com>
2026-06-18 10:49:48 -0300

    feat(TUS-2365): adiciona Lombok ao projeto e inicia o padrão de domínio

---
+++ src/main/java/org/example/domain/Student.java
@@ -0,0 +1,14 @@
+package org.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String studentID;
+    private String name;
+}
