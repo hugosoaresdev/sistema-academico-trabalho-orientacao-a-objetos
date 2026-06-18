2128223b6fd425372850c3f9ca1158db4f3d1779
Hugo Soares <hugosoareslopes1@gmail.com>
2026-06-18 11:33:32 -0300

    feat(TUS-2382): implementa equals e hashCode para o domínio

--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -2,13 +2,15 @@ package org.example.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "registrationNumber")
 public class Student {
-    private String studentID;
+    private String registrationNumber;
     private String name;
 }
